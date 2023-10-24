874ca2cebef172178634c37c7bdefe471579f17b
João Pedro Lopes <joaopalopes24@gmail.com>
2023-10-23 23:03:14 -0300

    feat(code): adding code components

---
+++ src/resources/views/components/code/preview.blade.php
@@ -0,0 +1,43 @@
+@props(['language', 'clean' => false, 'noCopy' => false, 'lineNumbers' => true, 'color' => false])
+
+<div x-cloak x-data="codePreview()"
+    {{ $attributes->class([
+        'relative shadow-md dark:shadow-none border-2 dark:border-0 rounded-lg' => !$clean,
+        'mt-3 mb-6',
+    ]) }}>
+    <div x-show="!code" @class([
+        'bg-gray-100 dark:bg-secondary-700' => $color && !$clean,
+        'bg-white dark:bg-secondary-800' => !$color && !$clean,
+        'p-6 rounded-lg' => !$clean,
+    ])>
+        @if (check_slot($slot))
+            <div {{ $slot->attributes }}>
+                {!! Blade::render(serialize_slot($slot), $this->all()) !!}
+            </div>
+        @else
+            {!! Blade::render(serialize_slot($slot), $this->all()) !!}
+        @endif
+    </div>
+
+    <div x-ref="code" x-show="code" class="level3" wire:ignore>
+        <x-docs-code :language="$language" :contents="serialize_slot($slot)" :line-numbers="$lineNumbers" />
+    </div>
+
+    <div class="absolute top-0 right-0 p-1">
+        <div class="flex items-center space-x-2">
+            @if (!$noCopy)
+                <x-mini-button x-show="code" x-ref="copy" primary flat focus="none" xs>
+                    <x-icon x-show="!copy" name="document" class="w-4 h-4 stroke-current" outline />
+
+                    <x-icon x-show="copy" name="document-check" class="w-4 h-4 stroke-current" outline />
+                </x-mini-button>
+            @endif
+
+            <x-mini-button x-on:click="toggleCode" primary flat focus="none" xs>
+                <x-icon x-show="code" name="eye" class="w-4 h-4 stroke-current" outline />
+
+                <x-icon x-show="!code" name="code-bracket" class="w-4 h-4 stroke-current" outline />
+            </x-mini-button>
+        </div>
+    </div>
+</div>
---
+++ src/resources/views/documentation/alert.blade.php
@@ -0,0 +1,99 @@
+<div>
+    <x-docs::code.preview language="blade">
+        @verbatim
+            <div class="flex justify-center gap-4">
+                <x-alert title="Lorem Ipsum is simply!" padding="px-2" primary flat>
+                    This is an error alert — <b>check it out!</b>
+                </x-alert>
+            </div>
+
+            <div class="flex justify-center gap-4 mt-4">
+                <x-alert title="Lorem Ipsum is simply!" padding="px-2" primary outline>
+                    This is an error alert — <b>check it out!</b>
+                </x-alert>
+            </div>
+
+            <div class="flex justify-center gap-4 mt-4">
+                <x-alert title="Lorem Ipsum is simply!" padding="px-2" primary solid>
+                    This is an error alert — <b>check it out!</b>
+                </x-alert>
+            </div>
+
+
+            <div class="flex justify-center gap-4 mt-4">
+                <x-alert title="Lorem Ipsum is simply!" padding="px-2" positive flat>
+                    This is an error alert — <b>check it out!</b>
+                </x-alert>
+            </div>
+
+            <div class="flex justify-center gap-4 mt-4">
+                <x-alert title="Lorem Ipsum is simply!" padding="px-2" positive outline>
+                    This is an error alert — <b>check it out!</b>
+                </x-alert>
+            </div>
+
+            <div class="flex justify-center gap-4 mt-4">
+                <x-alert title="Lorem Ipsum is simply!" padding="px-2" positive solid>
+                    This is an error alert — <b>check it out!</b>
+                </x-alert>
+            </div>
+
+
+            <div class="flex justify-center gap-4 mt-4">
+                <x-alert title="Lorem Ipsum is simply!" padding="px-2" negative flat>
+                    This is an error alert — <b>check it out!</b>
+                </x-alert>
+            </div>
+
+            <div class="flex justify-center gap-4 mt-4">
+                <x-alert title="Lorem Ipsum is simply!" padding="px-2" negative outline>
+                    This is an error alert — <b>check it out!</b>
+                </x-alert>
+            </div>
+
+            <div class="flex justify-center gap-4 mt-4">
+                <x-alert title="Lorem Ipsum is simply!" padding="px-2" negative solid>
+                    This is an error alert — <b>check it out!</b>
+                </x-alert>
+            </div>
+
+
+            <div class="flex justify-center gap-4 mt-4">
+                <x-alert title="Lorem Ipsum is simply!" padding="px-2" warning flat>
+                    This is an error alert — <b>check it out!</b>
+                </x-alert>
+            </div>
+
+            <div class="flex justify-center gap-4 mt-4">
+                <x-alert title="Lorem Ipsum is simply!" padding="px-2" warning outline>
+                    This is an error alert — <b>check it out!</b>
+                </x-alert>
+            </div>
+
+            <div class="flex justify-center gap-4 mt-4">
+                <x-alert title="Lorem Ipsum is simply!" padding="px-2" warning solid>
+                    This is an error alert — <b>check it out!</b>
+                </x-alert>
+            </div>
+
+
+            <div class="flex justify-center gap-4 mt-4">
+                <x-alert title="Lorem Ipsum is simply!" padding="px-2" info flat>
+                    This is an error alert — <b>check it out!</b>
+                </x-alert>
+            </div>
+
+            <div class="flex justify-center gap-4 mt-4">
+                <x-alert title="Lorem Ipsum is simply!" padding="px-2" info outline>
+                    This is an error alert — <b>check it out!</b>
+                </x-alert>
+            </div>
+
+            <div class="flex justify-center gap-4 mt-4">
+                <x-alert title="Lorem Ipsum is simply!" padding="px-2" info solid>
+                    This is an error alert — <b>check it out!</b>
+                </x-alert>
+            </div>
+        @endverbatim
+    </x-docs::code.preview>
+</div>
