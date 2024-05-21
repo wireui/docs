f84adae171d80b3269cfa26baf83cc9162cc3b8c
João Pedro Lopes <joaopalopes24@gmail.com>
2024-05-21 19:49:39 -0300

    wip

--- src/resources/views/sections/packages/breadcrumbs.blade.php
+++ src/resources/views/sections/packages/breadcrumbs.blade.php
@@ -1,9 +1,91 @@
 <div>
     <x-slot name="summary">
         <x-docs::summary>
-            <!--  -->
+            <x-docs::summary.header href="#requirements" label="Requirements" />
+
+            <x-docs::summary.header href="#installation" label="Installation" />
+
+            <x-docs::summary.header href="#publishing" label="Publishing" />
+
+            <x-docs::summary.header href="#configure" label="Configure" />
+
+            <x-docs::summary.header href="#how-to-use-it" label="How to use it?" />
+
+            <x-docs::summary.header href="#directive-api" label="Directive API" />
         </x-docs::summary>
     </x-slot>
 
     <x-docs::title id="breadcrumbs" title="Breadcrumbs" />
+
+    <x-docs::subtitle id="requirements" title="Requirements" />
+
+    <x-docs::text>
+        Requirements
+    </x-docs::text>
+
+    <x-docs::list>
+        <x-docs::list.item>
+            <x-link href="https://www.php.net" label="PHP 8.2" target="_blank" underline="none" teal />
+        </x-docs::list.item>
+    </x-docs::list>
+
+    <x-docs::subtitle id="installation" title="Installation" />
+
+    <x-docs::text>
+        Installation
+    </x-docs::text>
+
+    <x-docs::code.block language="bash">
+        @verbatim
+            Installation
+        @endverbatim
+    </x-docs::code.block>
+
+    <x-docs::subtitle id="publishing" title="Publishing" />
+
+    <x-docs::text>
+        Publishing
+    </x-docs::text>
+
+    <x-docs::code.block language="bash">
+        @verbatim
+            Publishing
+        @endverbatim
+    </x-docs::code.block>
+
+    <x-docs::subtitle id="configure" title="Configure" />
+
+    <x-docs::text>
+        Configure
+    </x-docs::text>
+
+    <x-docs::code.block language="js">
+        @verbatim
+            Configure
+        @endverbatim
+    </x-docs::code.block>
+
+    <x-docs::subtitle id="how-to-use-it" title="How to use it?" />
+
+    <x-docs::text>
+        How to use it?
+    </x-docs::text>
+
+    <x-docs::code.block language="js">
+        @verbatim
+            How to use it?
+        @endverbatim
+    </x-docs::code.block>
+
+    <x-docs::subtitle id="directive-api" title="Directive API" />
+
+    <x-docs::text>
+        Directive API
+    </x-docs::text>
+
+    <x-docs::code.block language="js">
+        @verbatim
+            Directive API
+        @endverbatim
+    </x-docs::code.block>
 </div>
