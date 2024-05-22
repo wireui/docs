a7e03585f990007463dcc0098634bbf64789367e
João Pedro Lopes <joaopalopes24@gmail.com>
2024-05-21 22:55:06 -0300

    wip

--- tests/Unit/Facades/WireUiDocsTest.php
+++ tests/Unit/Facades/WireUiDocsTest.php
@@ -1,5 +1,21 @@
 <?php
 
-test('example', function () {
+test('it should check sections', function () {
+    expect(true)->toBeTrue();
+});
+
+test('it should check default pages', function () {
+    expect(true)->toBeTrue();
+});
+
+test('it should check components api', function () {
+    expect(true)->toBeTrue();
+});
+
+test('it should check pages', function () {
+    expect(true)->toBeTrue();
+});
+
+test('it should check links', function () {
     expect(true)->toBeTrue();
 });
